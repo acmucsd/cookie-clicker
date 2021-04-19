51e43b71cc201db0047201e1c8c49c6456c2b592
Paul Pan <panjunhong05@gmail.com>
2021-04-18 18:32:31 -0700

    Added timing.

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const initialState = {
 };
 
 const reducer = (state: any, action: any) => {
-  switch (action.state) {
+  switch (action.type) {
     case 'update-structures':
       return { numStructures: action.numStructures, ...action.state };
     case 'update-cps':
@@ -29,6 +29,15 @@ const App: React.FC = () => {
   const AppContext = React.createContext({ state: contextState, updater: updateContext });
   React.useEffect(() => {
     updateContext(localStorage.getItem('state'));
+    if (localStorage.getItem('time') !== null) {
+      const duration = new Date().getMilliseconds() - parseInt(localStorage.getItem('time')!, 10);
+      if (duration > 10000) {
+        document.getElementById('welcome-back')!.style.display = 'block';
+        document.getElementById(
+          'welcome-back-text',
+        )!.innerHTML = `Welcome back, you were offline for ${Math.floor(duration / 1000)} seconds.`;
+      }
+    }
   }, []);
   setInterval(() => {
     updateContext({ type: 'update-structures', numStructures: contextState.cps * i });
@@ -37,10 +46,15 @@ const App: React.FC = () => {
   setInterval(() => {
     saveState(contextState);
   }, 1000);
+  setInterval(() => {
+    localStorage.setItem('time', new Date().getMilliseconds().toString());
+  }, 1000);
   return (
-    <AppContext.Provider
-      value={{ state: contextState, updater: updateContext }}
-    ></AppContext.Provider>
+    <AppContext.Provider value={{ state: contextState, updater: updateContext }}>
+      <div id="welcome-back" style={{ display: 'none' }}>
+        <span id="welcome-back-text"></span>
+      </div>
+    </AppContext.Provider>
   );
 };
 
